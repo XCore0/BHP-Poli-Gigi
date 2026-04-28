01c1df08ff69bda4597a9b7ee5d0f0de48083b48
Rivrrr <fatquliman1806@gmail.com>
2026-04-28 13:55:38 +0700

    tambah sql

--- db_poli_gigi (1).sql
+++ db_poli_gigi (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 23, 2026 at 02:45 PM
+-- Generation Time: Apr 28, 2026 at 06:54 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.3.30
 
@@ -32,7 +32,7 @@ CREATE TABLE `bhp` (
   `Kode_bhp` varchar(20) DEFAULT NULL,
   `Nama_bhp` varchar(100) NOT NULL,
   `Jumlah` int DEFAULT '0',
-  `Pemakaian` varchar(12) DEFAULT NULL,
+  `Pemakaian` varchar(12) NOT NULL,
   `id_kategori` int UNSIGNED DEFAULT NULL,
   `id_satuan` int UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -45,9 +45,16 @@ CREATE TABLE `bhp` (
 
 CREATE TABLE `kategori_bhp` (
   `id_kategori` int UNSIGNED NOT NULL,
-  `Kode_kategori` varchar(20) DEFAULT NULL,
+  `Kode_kategori` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `Nama_kategori` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `kategori_bhp`
+--
+
+INSERT INTO `kategori_bhp` (`id_kategori`, `Kode_kategori`, `Nama_kategori`) VALUES
+(1, NULL, 'Alat Pelindung');
 
 -- --------------------------------------------------------
 
@@ -86,7 +93,23 @@ INSERT INTO `log_aktivitas` (`id_log`, `id_user`, `nama_user`, `role_user`, `aks
 (12, 4, 'Fatqul Iman', 'admin', 'login', 'auth', 'Fatqul Iman berhasil masuk ke sistem.', '::1', '2026-04-22 13:30:07'),
 (13, 4, 'Fatqul Iman', 'admin', 'login', 'auth', 'Fatqul Iman berhasil masuk ke sistem.', '::1', '2026-04-23 21:01:35'),
 (14, 4, 'Fatqul Iman', 'admin', 'logout', 'auth', 'Fatqul Iman keluar dari sistem.', '::1', '2026-04-23 21:02:07'),
-(15, 4, 'Fatqul Iman', 'admin', 'login', 'auth', 'Fatqul Iman berhasil masuk ke sistem.', '::1', '2026-04-23 21:18:26');
+(15, 4, 'Fatqul Iman', 'admin', 'login', 'auth', 'Fatqul Iman berhasil masuk ke sistem.', '::1', '2026-04-23 21:18:26'),
+(16, 4, 'Fatqul Iman', 'admin', 'tambah_kategori', 'bhp', 'Menambahkan kategori: Alat Pelindung.', '::1', '2026-04-23 22:48:05'),
+(17, 4, 'Fatqul Iman', 'admin', 'tambah_satuan', 'bhp', 'Menambahkan satuan: AMPULE.', '::1', '2026-04-23 22:48:28'),
+(18, 1, 'Admin', 'admin', 'tambah_bhp', 'bhp', 'Testing add_bhp log', '0.0.0.0', '2026-04-26 19:26:23'),
+(19, 6, 'Edo', 'dokter', 'login', 'auth', 'Edo berhasil masuk ke sistem.', '::1', '2026-04-26 19:29:47'),
+(20, 6, 'Edo', 'dokter', 'logout', 'auth', 'Edo keluar dari sistem.', '::1', '2026-04-26 19:30:07'),
+(21, 4, 'Fatqul Iman', 'admin', 'login', 'auth', 'Fatqul Iman berhasil masuk ke sistem.', '::1', '2026-04-26 19:30:13'),
+(22, 4, 'Fatqul Iman', 'admin', 'tambah_bhp', 'bhp', 'Menambahkan BHP: ADRENALINE (kode: BHP 001).', '::1', '2026-04-26 19:30:34'),
+(23, 4, 'Fatqul Iman', 'admin', 'edit_bhp', 'bhp', 'Mengedit BHP ID 2: ADRENALINE.', '::1', '2026-04-26 19:30:49'),
+(24, 4, 'Fatqul Iman', 'admin', 'logout', 'auth', 'Fatqul Iman keluar dari sistem.', '::1', '2026-04-26 19:59:36'),
+(25, 4, 'Fatqul Iman', 'admin', 'login', 'auth', 'Fatqul Iman berhasil masuk ke sistem.', '::1', '2026-04-27 19:17:52'),
+(26, 4, 'Fatqul Iman', 'admin', 'login', 'auth', 'Fatqul Iman berhasil masuk ke sistem.', '::1', '2026-04-28 11:15:56'),
+(27, 4, 'Fatqul Iman', 'admin', 'hapus_bhp', 'bhp', 'Menghapus BHP ID 1.', '::1', '2026-04-28 12:46:32'),
+(28, 4, 'Fatqul Iman', 'admin', 'hapus_bhp', 'bhp', 'Menghapus BHP ID 2.', '::1', '2026-04-28 12:48:09'),
+(29, 4, 'Fatqul Iman', 'admin', 'tambah_bhp', 'bhp', 'Menambahkan BHP: apd (kode: bhp 200).', '::1', '2026-04-28 12:49:47'),
+(30, 4, 'Fatqul Iman', 'admin', 'edit_bhp', 'bhp', 'Mengedit BHP ID 3: APD.', '::1', '2026-04-28 12:50:11'),
+(31, 4, 'Fatqul Iman', 'admin', 'hapus_bhp', 'bhp', 'Menghapus BHP ID 3.', '::1', '2026-04-28 12:50:17');
 
 -- --------------------------------------------------------
 
@@ -98,6 +121,13 @@ CREATE TABLE `satuan_bhp` (
   `id_satuan` int UNSIGNED NOT NULL,
   `Nama_satuan` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `satuan_bhp`
+--
+
+INSERT INTO `satuan_bhp` (`id_satuan`, `Nama_satuan`) VALUES
+(1, 'AMPULE');
 
 -- --------------------------------------------------------
 
@@ -141,7 +171,8 @@ ALTER TABLE `bhp`
 -- Indexes for table `kategori_bhp`
 --
 ALTER TABLE `kategori_bhp`
-  ADD PRIMARY KEY (`id_kategori`);
+  ADD PRIMARY KEY (`id_kategori`),
+  ADD UNIQUE KEY `Kode_kategory` (`Kode_kategori`);
 
 --
 -- Indexes for table `log_aktivitas`
@@ -174,25 +205,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `bhp`
 --
 ALTER TABLE `bhp`
-  MODIFY `id_bhp` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_bhp` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `kategori_bhp`
 --
 ALTER TABLE `kategori_bhp`
-  MODIFY `id_kategori` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kategori` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `log_aktivitas`
 --
 ALTER TABLE `log_aktivitas`
-  MODIFY `id_log` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_log` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `satuan_bhp`
 --
 ALTER TABLE `satuan_bhp`
-  MODIFY `id_satuan` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_satuan` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
